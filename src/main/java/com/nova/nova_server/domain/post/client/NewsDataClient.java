914641d0f60ef570aae5c8ffabf99988fab89fcb
은지 <ijnuegnaj@gmail.com>
2026-01-19 23:34:25 +0900

    주석 수정

--- src/main/java/com/nova/nova_server/domain/post/client/NewsDataClient.java
+++ src/main/java/com/nova/nova_server/domain/post/client/NewsDataClient.java
@@ -1,7 +1,5 @@
 package com.nova.nova_server.domain.post.client;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
