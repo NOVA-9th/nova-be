d78f99b464b5c9944dc1b6f166afa5a9404fc5ef
AlphaBs <ksi123456ab@gmail.com>
2026-01-31 23:31:12 +0900

    작업중

--- src/main/java/com/nova/nova_server/domain/cardType/repository/CardTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nova.nova_server.domain.cardType.repository;
-
-import com.nova.nova_server.domain.cardType.entity.CardType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardTypeRepository extends JpaRepository<CardType, Long> {
-}
