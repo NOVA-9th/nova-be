cb0fea98b4b2395a0f3812b68e66a67dd1b029cf
Sein Kwon <ksi123456ab@gmail.com>
2026-02-07 01:35:52 +0900

    [REFACTOR] 프로필 이미지 로직 리팩토링 (#57)
    * refactor
    
    * work
    
    * fix

--- src/main/java/com/nova/nova_server/domain/member/dto/MemberResponseDto.java
+++ src/main/java/com/nova/nova_server/domain/member/dto/MemberResponseDto.java
@@ -1,5 +1,6 @@
 package com.nova.nova_server.domain.member.dto;
 
+import com.nova.nova_server.domain.member.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
