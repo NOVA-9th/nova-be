239b731a9985f157cbca40878c62b3fefe9eb5d9
devgeon <devgeon.git@gmail.com>
2026-02-09 00:38:08 +0900

    [FEAT] 유저 관심 키워드 조회 API 추가

--- src/main/java/com/nova/nova_server/domain/member/service/MemberService.java
+++ src/main/java/com/nova/nova_server/domain/member/service/MemberService.java
@@ -2,20 +2,19 @@ package com.nova.nova_server.domain.member.service;
 
 import com.nova.nova_server.domain.interest.entity.Interest;
 import com.nova.nova_server.domain.interest.repository.InterestRepository;
+import com.nova.nova_server.domain.keyword.entity.Keyword;
 import com.nova.nova_server.domain.keyword.error.KeywordErrorCode;
+import com.nova.nova_server.domain.keyword.repository.KeywordRepository;
 import com.nova.nova_server.domain.member.dto.*;
 import com.nova.nova_server.domain.member.entity.Member;
-import com.nova.nova_server.domain.keyword.entity.Keyword;
-import com.nova.nova_server.domain.keyword.repository.KeywordRepository;
 import com.nova.nova_server.domain.member.entity.MemberPreferInterest;
 import com.nova.nova_server.domain.member.entity.MemberPreferKeyword;
 import com.nova.nova_server.domain.member.error.MemberErrorCode;
 import com.nova.nova_server.domain.member.repository.MemberPreferInterestRepository;
 import com.nova.nova_server.domain.member.repository.MemberPreferKeywordRepository;
-import com.nova.nova_server.domain.member.repository.MemberRepository;
 import com.nova.nova_server.domain.member.repository.MemberProfileImageRepository;
+import com.nova.nova_server.domain.member.repository.MemberRepository;
 import com.nova.nova_server.global.apiPayload.exception.NovaException;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -111,6 +110,10 @@ public class MemberService {
         // 키워드
         List<String> keywordNames = memberPersonalizationDto.keywords();
         updateMemberKeywords(member, keywordNames);
+    }
+
+    public List<String> getMemberKeywords(Long memberId) {
+        return memberPreferKeywordRepository.findKeywordNamesByMemberId(memberId);
     }
 
     private void updateMemberInterests(Member member, List<Long> interestIds) {
