a7df564b98939a24a92085eb700bcf0f909ba642
devgeon <devgeon.git@gmail.com>
2026-02-09 22:53:57 +0900

    [CHORE] 관련도 점수 최대값에 대한 주석 추가

--- src/main/java/com/nova/nova_server/global/config/FeedConfig.java
+++ src/main/java/com/nova/nova_server/global/config/FeedConfig.java
@@ -11,6 +11,13 @@ public class FeedConfig {
     @Value("${feed.max-page-size:10}")
     private int maxPageSize;
 
+    /**
+     * 피드 최대 점수<br/>
+     * - 기본 점수: 50<br/>
+     * - 키워드 매칭 시: +10<br/>
+     * - 북마크된 카드 뉴스와 키워드 매칭 시: +5<br/>
+     * 카드 뉴스 하나 당 키워드는 최대 5개이므로 최대 점수는 125점
+     */
     @Value("${feed.max-score:125}")
     private int maxScore;
 
